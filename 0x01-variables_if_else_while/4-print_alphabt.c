f43474d3b9c0b19c0c01e7ad7a706fffbd29aca0
Lovayi <kevinphakathi@live.com>
2022-06-03 03:32:17 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,49 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - Prints the alphabet without q and e.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, e, g, h, op1, op2;
+	int i;
 
-i = e = g = h = 48;
-while (h < 58)
-{
-	g = 48;
-	while (g < 58)
+	for (i = 97; i < 123; i++)
 	{
-		e = 48;
-		while (e < 58)
+		if (i != 101 && i != 113)
 		{
-			i = 48;
-			while (i < 58)
-			{
-				op1 = (h * 10) + g;
-				op2 = (e * 10) + i;
-				if (op1 < op2)
-				{
-					putchar(h);
-					putchar(g);
-					putchar(' ');
-					putchar(e);
-					putchar(i);
-					if (h == 57 && g == 56 && e == 57 && i == 57)
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-				i++;
-			}
-			e++;
+			putchar(i);
 		}
-		g++;
 	}
-	h++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
 
