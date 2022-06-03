85c5c76f0af7a7276c042c74857c364552d74780
Lovayi <kevinphakathi@live.com>
2022-06-03 03:43:20 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,49 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - Prints numbers between 0 to 9.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, e, g, h, op1, op2;
+	int i;
 
-i = e = g = h = 48;
-while (h < 58)
-{
-	g = 48;
-	while (g < 58)
+	for (i = 48; i < 58; i++)
 	{
-		e = 48;
-		while (e < 58)
-		{
-			i = 48;
-			while (i < 58)
-			{
-				op1 = (h * 10) + g;
-				op2 = (e * 10) + i;
-				if (op1 < op2)
-				{
-					putchar(h);
-					putchar(g);
-					putchar(' ');
-					putchar(e);
-					putchar(i);
-					if (h == 57 && g == 56 && e == 57 && i == 57)
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-				i++;
-			}
-			e++;
-		}
-		g++;
+		putchar(i);
 	}
-	h++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
 
